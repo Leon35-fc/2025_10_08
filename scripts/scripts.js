fabe36bab510e4f20dc31d5c3cfb73ef1874600a
Leon35-fc <leon.f35@alice.it>
2025-10-08 16:58:56 +0200

    Text "completed" missing

---
+++ scripts/scripts.js
@@ -0,0 +1,44 @@
+//recupero il riferimento dal form
+const form = document.getElementById('cosaFaro')
+
+//funzione per eliminare un'attività
+const eliminaAttivita = function (e){
+    console.log('ELIMINO CARD', e)
+
+    //sintassi: elimina il padre dell'oggetto target(il button) che ha scatenato l'evento
+    e.target.parentElement.remove()
+}
+
+//recupero le informazioni dal form
+const gestisciAttivita = function (e) {
+    //impedisce l'aggiornamento della pagina quando viene richiamata la funzione
+    e.preventDefault()
+    console.log('RECUPERIAMO I DATI')
+
+    //recupero il riferimento all'input
+    const recuperoAttivita = document.getElementById('attivitaDaAggiungere') 
+
+    //recupero il valore dal campo input
+    const qualeAttivita = recuperoAttivita.value
+    console.log("L'attività da fare è :", qualeAttivita)  
+    
+    //creo div vuoto che contiene i dati della mia attività ed un pulsante per eliminarla
+    const scheda = document.createElement('div')
+    
+    //applico una regola css ad applicare a tutti i div delle attività
+    scheda.classList.add('scheda-attivita')
+
+    //aggiungo i dati del form al div
+    scheda.innerHTML = `
+    <p>${qualeAttivita}</p>
+        <button onclick="eliminaAttivita(event)">Elimina</button>
+    `
+    // aggiungiamo la nuova scheda alla lista attività
+    const container = document.getElementById('listaAttivita')
+    container.appendChild(scheda)
+
+    //reset dei campi del form
+    form.reset()
+}
+
+form.addEventListener('submit', gestisciAttivita)
